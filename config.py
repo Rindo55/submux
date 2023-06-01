80aa4907bd59c6b9ab64b7dce4c349519ad5bb28
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-01 20:00:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config(object):
    
     BOT_TOKEN = "5410320498:AAGAPfA_Wnl4ZQRpCIWhxQR008O97_aIf7I"
     TELEGRAM_API = 10247139
-    OWNER = 1443454117
-    OWNER_USERNAME = "Pirate_User"
+    OWNER = 1425489930
+    OWNER_USERNAME = "Pirate_user"
     PASSWORD = "Sub"
     DATABASE_URL = "mongodb+srv://erai:raws@cluster1.zefvxlt.mongodb.net/?retryWrites=true&w=majority"
     LOGCHANNEL = -1001557165553  # Add channel id as -100 + Actual ID
